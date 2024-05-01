3eb48386f01fde70892e8e83793c6a6ac807cbb4
Amir Saman Mirjalili <57065409+AmirSamanMirjalili@users.noreply.github.com>
2024-05-01 15:22:22 +0330

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/amir/Programming/CppTest/LIRM_project/simple_2d_object
-# Build directory: /home/amir/Programming/CppTest/LIRM_project/simple_2d_object/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(my-cpp-project-tests "/home/amir/Programming/CppTest/LIRM_project/simple_2d_object/cmake-build-debug/tests")
-set_tests_properties(my-cpp-project-tests PROPERTIES  _BACKTRACE_TRIPLES "/home/amir/Programming/CppTest/LIRM_project/simple_2d_object/CMakeLists.txt;32;add_test;/home/amir/Programming/CppTest/LIRM_project/simple_2d_object/CMakeLists.txt;0;")
-subdirs("lib/googletest")
--- cmake-build-debug/lib/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/amir/Programming/CppTest/LIRM_project/simple_2d_object/lib/googletest
-# Build directory: /home/amir/Programming/CppTest/LIRM_project/simple_2d_object/cmake-build-debug/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
